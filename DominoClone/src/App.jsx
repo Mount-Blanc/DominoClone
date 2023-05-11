842747163277bf6de8f7eca299f171bcb84cd841
Mount-Blanc <mountblanc100@gmail.com>
2023-05-11 15:25:46 -0500

    added header elements

--- DominoClone/src/App.jsx
+++ DominoClone/src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  <header>
+    <div>
+      <nav className="primary-nav">
+        <ul>
+          <li>
+            <a href="">Order Online</a>
+          </li>
+          <li>
+            <a href="">Locations</a>
+          </li>
+          <li>
+            <a href="">Menu</a>
+          </li>
+          <li>
+            <a href="">Coupons</a>
+          </li>
+          <li>
+            <a href="">Tracker</a>
+          </li>
+          <li>
+            <a href="">Rewards</a>
+          </li>
+        </ul>
+        <ul>
+          <li>Earn Points</li>
+          <li>Sign in and Earn Rewards</li>
+          <li>Cart</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
     </>
   )
 }
